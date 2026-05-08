4308df3436bceb88c780e040023f07f8eba434e7
Nexusscript <figueroamorillojaydenalexis@gmail.com>
2026-05-08 02:43:54 -0400

    Initialize Discord bot with message responses
    This code sets up a Discord bot that responds to specific commands.

---
+++ Node.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot conectado como ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong!');
+  }
+
+  if (message.content === '!youtube') {
+    message.reply('🎥 Bienvenido al sistema de YouTubers Manager!');
+  }
+});
+
+client.login('1502193893209411584');
